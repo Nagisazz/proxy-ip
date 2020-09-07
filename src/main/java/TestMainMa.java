d6a33095c4d0e9e7ca8cb5b3bc7412e44c7ab2a2
zhushengzhe <zhushengzhe@thunisoft.com>
2020-09-07 15:02:30 +0800

    测试ma

--- src/main/java/TestMainMa.java
+++
@@ -1,6 +0,0 @@
-/**
- * @auther zhushengzhe
- * @date 2020/9/7 14:39
- */
-public class TestMainMa {
-}
--- src/main/java/TestMainMa2.java
+++
@@ -1,6 +0,0 @@
-/**
- * @auther zhushengzhe
- * @date 2020/9/7 14:45
- */
-public class TestMainMa2 {
-}
