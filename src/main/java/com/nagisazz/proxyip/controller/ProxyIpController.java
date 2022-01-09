b43f57333c357925c58318ac7fe7486b4962d66a
NagisaZZ <597306107@qq.com>
2022-01-09 17:43:43 +0800

    [+]add 监控

--- src/main/java/com/nagisazz/proxyip/controller/ProxyIpController.java
+++ src/main/java/com/nagisazz/proxyip/controller/ProxyIpController.java
@@ -10,6 +10,9 @@ import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import javax.servlet.http.HttpServletRequest;
+import java.net.InetAddress;
+import java.net.UnknownHostException;
 import java.util.List;
 
 @Slf4j
@@ -24,12 +27,14 @@ public class ProxyIpController {
     private RecordValidProxy recordValidProxy;
 
     @GetMapping("/list/{size}")
-    public List<ProxyIpLog> list(@PathVariable Integer size) {
+    public List<ProxyIpLog> list(@PathVariable Integer size, HttpServletRequest request) {
+        log.info("访问list接口，访问ip：{}", getIpAddr(request));
         return proxyIpService.selectSizeList(size);
     }
 
     @GetMapping("/remove/{ip}/{port}")
-    public void remove(@PathVariable String ip, @PathVariable String port) {
+    public void remove(@PathVariable String ip, @PathVariable String port, HttpServletRequest request) {
+        log.info("访问remove接口，访问ip：{}", getIpAddr(request));
         proxyIpService.remove(ip + ":" + port);
     }
 
@@ -38,4 +43,44 @@ public class ProxyIpController {
 //        Set<String> strings = new HashSet<>(Collections.singletonList("1"));
 //        recordValidProxy.write(strings);
 //    }
+
+    private static final String UNKNOWN = "unknown";
+    private static final String LOCALHOST = "127.0.0.1";
+    private static final String SEPARATOR = ",";
+
+    public static String getIpAddr(HttpServletRequest request) {
+        System.out.println(request);
+        String ipAddress;
+        try {
+            ipAddress = request.getHeader("x-forwarded-for");
+            if (ipAddress == null || ipAddress.length() == 0 || UNKNOWN.equalsIgnoreCase(ipAddress)) {
+                ipAddress = request.getHeader("Proxy-Client-IP");
+            }
+            if (ipAddress == null || ipAddress.length() == 0 || UNKNOWN.equalsIgnoreCase(ipAddress)) {
+                ipAddress = request.getHeader("WL-Proxy-Client-IP");
+            }
+            if (ipAddress == null || ipAddress.length() == 0 || UNKNOWN.equalsIgnoreCase(ipAddress)) {
+                ipAddress = request.getRemoteAddr();
+                if (LOCALHOST.equals(ipAddress)) {
+                    InetAddress inet = null;
+                    try {
+                        inet = InetAddress.getLocalHost();
+                    } catch (UnknownHostException e) {
+                        e.printStackTrace();
+                    }
+                    ipAddress = inet.getHostAddress();
+                }
+            }
+            // 对于通过多个代理的情况，第一个IP为客户端真实IP,多个IP按照','分割
+            // "***.***.***.***".length()
+            if (ipAddress != null && ipAddress.length() > 15) {
+                if (ipAddress.indexOf(SEPARATOR) > 0) {
+                    ipAddress = ipAddress.substring(0, ipAddress.indexOf(","));
+                }
+            }
+        } catch (Exception e) {
+            ipAddress = "";
+        }
+        return ipAddress;
+    }
 }
